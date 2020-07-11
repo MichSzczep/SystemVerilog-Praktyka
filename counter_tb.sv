ba08653718919e2b1fb1b6ad67b88463d8bd2345
MichSzczep <57815651+MichSzczep@users.noreply.github.com>
2020-07-11 21:33:19 +0200

    Tasks added

--- counter_tb.sv
+++ counter_tb.sv
@@ -4,10 +4,32 @@
 
 module Counter_4bit_tb ( Counter_intface cint );
 
+ typedef enum logic [2:0] {A, B, C, D} TestCase;
+
+ TestCase c1;
+
+// inputs: 1. Reset is being set high or set low 2. When to start it
+// output: changed reset bit 
+task switch_reset (input bit on, input int del);				
+	#del cint.reset <= on;
+endtask
+
+// inputs: 1. Chnge is being set high or set low 2. When to start it
+// output: Chnge bit updated
+task switch_change (input bit on, input int del);
+	#del cint.chnge <= on;
+endtask
+
+
+// inputs: 1.Load value counter will start counting from, 2. When to start it
+// output: counter starts counting using load value as a starting number 
+task load_value (input logic [3:0] load, input int del);
+	#del cint.load <= load;
+endtask
 
 
  initial 
-  begin
+  begin : testing_obj
    cint.chnge <= 1;
    cint.reset <= 0; 
    cint.CLK <= 0;
@@ -20,13 +42,13 @@ module Counter_4bit_tb ( Counter_intface cint );
    #300 cint.load <= 11;
 
    #1000 $finish;
-  end
+  end : testing_obj
 
  always 
-  begin
+  begin : clock_ticks
    cint.CLK <= 0; #10;
    cint.CLK <= 1; #10;
-  end 
+  end : clock_ticks
 
 endmodule				
 
